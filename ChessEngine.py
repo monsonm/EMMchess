4e2f65300ae1c45eb1dcbb3d04ff847f85663a86
mausharte <78930480+mausharte@users.noreply.github.com>
2021-04-13 18:13:45 -0400

    Add files via upload

---
+++ ChessEngine.py
@@ -0,0 +1,19 @@
+"""
+This class will store all information regarding the current game state and for determining valid chess moves
+at the current game state. It will also keep a log of all moves.
+"""
+
+class GameState():
+    def __init__(self):
+        self.board =[
+            ["bR", "bN", "bB", "bQ", "bK", "bB", "bN", "bR"],
+            ["bP", "bP", "bP", "bP", "bP", "bP", "bP", "bP"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["--", "--", "--", "--", "--", "--", "--", "--"],
+            ["wP", "wP", "wP", "wP", "wP", "wP", "wP", "wP"],
+            ["wR", "wN", "wB", "wQ", "wK", "wB", "wN", "wR"]
+        ]
+        self.whiteToMove = True
+        self.moveLog = []
---
+++ ChessMain.py
@@ -0,0 +1,54 @@
+"""
+This class will be the main driver class and will handle user input and display of the current game state.
+"""
+
+import pygame as p
+from Chess import ChessEngine
+
+width = height = 512
+dimension = 8
+sqSize = height // dimension
+maxFPS = 15
+images = {}
+
+def loadImages():
+    pieces = ['wP', 'wR', 'wN', 'wB', 'wQ', 'wK', 'bP', 'bR', 'bN', 'bB', 'bQ', 'bK',]
+    for piece in pieces:
+        images[piece] = p.transform .scale(p.image.load("images/" + piece + ".png"), (sqSize, sqSize))
+
+def main():
+    p.init()
+    screen = p.display.set_mode((width, height))
+    clock = p.time.Clock()
+    screen.fill(p.Color("white"))
+    gameState = ChessEngine.GameState()
+    loadImages()
+    running = True
+    while running:
+        for e in p.event.get():
+            if e.type == p.QUIT:
+                running = False
+        drawGameState(screen, gameState)
+        clock.tick(maxFPS)
+        p.display.flip()
+
+def drawGameState(screen, gameState):
+    drawBaord(screen)
+    drawPieces(screen, gameState.board)
+
+def drawBoard(screen):
+    colors = [p.Color("white"), p.Color("gray")]
+    for r in range(dimension):
+        for c in range(dimension):
+            color = colors[((r+c) % 2)]
+            p.draw.rect(screen, color, p.Rect(c*sqSize, r*sqSize, sqSize, sqSize))
+
+def drawPieces(screen, board):
+    for r in range(dimension):
+        for c in range(dimension):
+            piece = board[r][c]
+            if piece != "--":
+                screen.blit(images[piece], p.Rect(c*sqSize, r*sqSize, sqSize, sqSize))
+
+if __name__ == "__main__":
+    main()
