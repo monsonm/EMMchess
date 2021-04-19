76b0cdaf100384c6e7c72c77579082d33eaff193
mark <monsonm@xavier.edu>
2021-04-19 15:32:43 -0400

    part 5 completed

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -113,25 +113,59 @@ class GameState():
        Get All Knight Moves For The Knight Located At Row, Col And Then Adds To Move List
        '''
     def getKnightMoves(self, r, c, moves):
-        pass
+        knightMoves = ((-2, -1), (-2, 1), (-1, -2), (-1, 2), (1, -2), (1, 2), (2, -1), (2, 1))
+        allyColor = "w" if self.whiteToMove else "b"
+        for m in knightMoves:
+            endRow = r + m[0]
+            endCol = c + m[1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor:  # not an ally piece ( empty or enemy)
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
+
 
     '''
        Get All Bishop Moves For The Bishop Located At Row, Col And Then Adds To Move List
        '''
     def getBishopMoves(self, r, c, moves):
-        pass
+        directions = ((-1, -1), (-1, 1), (1, -1), (1, 1))
+        enemyColor = "b" if self.whiteToMove else "w"
+        for d in directions:
+            for i in range(1, 8):
+                endRow = r + d[0] * i
+                endCol = c + d[1] * i
+                if 0 <= endRow < 8 and 0 <= endCol < 8:
+                    endPiece = self.board[endRow][endCol]
+                    if endPiece == "--":  # empty space valid
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                    elif endPiece[0] == enemyColor:  # enemy piece valid
+                        moves.append(Move((r, c), (endRow, endCol), self.board))
+                        break
+                    else:  #friendly piece valid
+                        break
+                else:  # off board
+                    break
 
     '''
        Get All Queen Moves For The Queen Located At Row, Col And Then Adds To Move List
        '''
     def getQueenMoves(self, r, c, moves):
-        pass
+        self.getRookMoves(r, c, moves)
+        self.getBishopMoves(r, c, moves)
 
     '''
        Get All King Moves For The King Located At Row, Col And Then Adds To Move List
        '''
     def getKingMoves(self, r, c, moves):
-        pass
+        kingMoves = ((-1, -1), (-1, 0), (-1, 1), (0, -1), (0, 1), (1, -1), (1, 0), (1, 1))
+        allyColor = "w" if self.whiteToMove else "b"
+        for i in range(8):
+            endRow = r + kingMoves[i][0]
+            endCol = c + kingMoves[i][1]
+            if 0 <= endRow < 8 and 0 <= endCol < 8:
+                endPiece = self.board[endRow][endCol]
+                if endPiece[0] != allyColor:  # not an ally piece ( empty or enemy )
+                    moves.append(Move((r, c), (endRow, endCol), self.board))
 
 
 class Move():
