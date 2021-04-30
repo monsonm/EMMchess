06e9e0157db81462a5ac4eb59e61f9cf5477690b
mausharte <mausharte@gmail.com>
2021-04-29 20:33:23 -0400

    Continued implementation of Board, Piece, and individual piece classes.

--- Chess/ChessEngine.py
+++ Chess/ChessEngine.py
@@ -409,12 +409,22 @@ class Board():
         Squares.append(col)
 
     def __init__(self):
-        pass
+        self.board = [
+            [Square(RookPiece('b'), self, (0,0)), Square(KnightPiece('b'), self, (0,1)), Square(BishopPiece('b'), self, (0,2)), Square(QueenPiece('b'), self, (0,3)), Square(KingPiece('b'), self, (0,4)), Square(BishopPiece('b'), self, (0,5)), Square(KnightPiece('b'), self, (0,6)), Square(RookPiece('b'), self, (0,7))],
+            [Square(PawnPiece('b'), self, (1,0)), Square(PawnPiece('b'), self, (1,1)), Square(PawnPiece('b'), self, (1,2)), Square(PawnPiece('b'), self, (1,3)), Square(PawnPiece('b'), self, (1,4)), Square(PawnPiece('b'), self, (1,5)), Square(PawnPiece('b'), self, (1,6)), Square(PawnPiece('b'), self, (1,7))],
+            [Square(EmptyPiece(), self, (2,0)), Square(EmptyPiece(), self, (2,1)), Square(EmptyPiece(), self, (2,2)), Square(EmptyPiece(), self, (2,3)), Square(EmptyPiece(), self, (2,4)), Square(EmptyPiece(), self, (2,5)), Square(EmptyPiece(), self, (2,6)), Square(EmptyPiece(), self, (2,7))],
+            [Square(EmptyPiece(), self, (3,0)), Square(EmptyPiece(), self, (3,1)), Square(EmptyPiece(), self, (3,2)), Square(EmptyPiece(), self, (3,3)), Square(EmptyPiece(), self, (3,4)), Square(EmptyPiece(), self, (3,5)), Square(EmptyPiece(), self, (3,6)), Square(EmptyPiece(), self, (3,7))],
+            [Square(EmptyPiece(), self, (4,0)), Square(EmptyPiece(), self, (4,1)), Square(EmptyPiece(), self, (4,2)), Square(EmptyPiece(), self, (4,3)), Square(EmptyPiece(), self, (4,4)), Square(EmptyPiece(), self, (4,5)), Square(EmptyPiece(), self, (4,6)), Square(EmptyPiece(), self, (4,7))],
+            [Square(EmptyPiece(), self, (5,0)), Square(EmptyPiece(), self, (5,1)), Square(EmptyPiece(), self, (5,2)), Square(EmptyPiece(), self, (5,3)), Square(EmptyPiece(), self, (5,4)), Square(EmptyPiece(), self, (5,5)), Square(EmptyPiece(), self, (5,6)), Square(EmptyPiece(), self, (5,7))],
+            [Square(PawnPiece('w'), self, (6,0)), Square(PawnPiece('w'), self, (6,1)), Square(PawnPiece('w'), self, (6,2)), Square(PawnPiece('w'), self, (6,3)), Square(PawnPiece('w'), self, (6,4)), Square(PawnPiece('w'), self, (6,5)), Square(PawnPiece('w'), self, (6,6)), Square(PawnPiece('w'), self, (6,7))],
+            [Square(RookPiece('w'), self, (7,0)), Square(KnightPiece('w'), self, (7,1)), Square(BishopPiece('w'), self, (7,2)), Square(QueenPiece('w'), self, (7,3)), Square(KingPiece('w'), self, (7,4)), Square(BishopPiece('w'), self, (7,5)), Square(KnightPiece('w'), self, (7,6)), Square(RookPiece('w'), self, (7,7))]]
 
 
 class Square():
-    def __init__(self, piece):
+    def __init__(self, piece, board, position):
         self.piece = piece
+        self.board = board
+        self.position = position
 
     def changePiece(self, piece):
         self.piece = piece
@@ -423,50 +433,63 @@ class Square():
         return self.piece.getAllPossibleMoves()
 
 class Piece():
-    def __init__(self):
-        pass
+    def __init__(self, color):
+        self.color = color
 
     @abstractmethod
     def getAllPossibleMoves(self):
         pass
 
 class EmptyPiece(Piece):
-
+    def __init__(self):
+        super().__init__("-")
     def getAllPossibleMoves(self):
         pass
 
 
 class PawnPiece(Piece):
+    def __init__(self, color):
+        super().__init__(color)
 
     def getAllPossibleMoves(self):
         pass
 
 
 class RookPiece(Piece):
+    def __init__(self, color):
+        super().__init__(color)
 
     def getAllPossibleMoves(self):
         pass
 
 
 class KingPiece(Piece):
+    def __init__(self, color):
+        super().__init__(color)
 
     def getAllPossibleMoves(self):
         pass
 
 
 class QueenPiece(Piece):
+    def __init__(self, color):
+        super().__init__(color)
 
     def getAllPossibleMoves(self):
         pass
 
 
 class BishopPiece(Piece):
+    def __init__(self, color):
+        super().__init__(color)
 
     def getAllPossibleMoves(self):
         pass
 
 
 class KnightPiece(Piece):
+    def __init__(self, color):
+        super().__init__(color)
 
     def getAllPossibleMoves(self):
         pass
